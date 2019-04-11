ec640eb17bbac536c150aaddd81703f423a4ba72
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-04-10 18:16:21 -0600

    Create upgradepath.sh

---
+++ scripts/cron/upgradepath.sh
@@ -0,0 +1,29 @@
+# This is a WIP!
+
+# Check what version is intalled
+
+# ...
+
+# Check if necessary apps are installed
+
+APPLIST="acl apt-transport-https ca-certificates curl fail2ban fuse git gpg-agent grsync jq mergerfs nano rsyncufw socat unzip wget"
+
+for i in $APPLIST; do
+	echo Installing $i...
+	echo
+	sudo apt-get -y install $i
+	echo; sleep 2
+done
+
+# Upgrade Rclone service 
+
+sudo mv /etc/systemd/system/rclone* /tmp
+sudo rsync -a /opt/Gooby/scripts/services/rclonefs* /etc/systemd/system/
+sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/rclonefs.service
+
+cat $HOME/.config/rclone/rclone.conf | grep "[Local]" > /dev/null
+if ! [[ ${?} -eq 0 ]]; then
+	echo [Local] >> $HOME/.config/rclone/rclone.conf
+	echo type = local >> $HOME/.config/rclone/rclone.conf
+	echo nounc = >> $HOME/.config/rclone/rclone.conf
+fi
